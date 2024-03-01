992f6da113a5628d1e3b1084c6f03f6b1ad18a59
Rafael Mota Alves <127457574+RafaelMotaEDU@users.noreply.github.com>
2024-03-01 15:37:13 -0300

    adicionando inputs para os valores de `a` e `b`

---
+++ M1/Aula_01_29_02_2024/exercicio02.m
@@ -0,0 +1,11 @@
+% Resolve o Teorema de Pitagoras
+
+clear; clc;
+
+a = input('Digite o valor do A: ');
+b = input('Digite o valor do B: ');
+
+c = sqrt(a^2 + b^2);
+
+disp('O valor da Hipotenusa é = ');
+disp(c)
