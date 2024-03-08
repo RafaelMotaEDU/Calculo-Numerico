bdb53e95d61dce123566d156ac80dd60ec337912
Rafael Mota Alves <127457574+RafaelMotaEDU@users.noreply.github.com>
2024-03-07 21:56:22 -0300

    utilizando condicionais para resultado da media final

---
+++ M1/Aula_02_07_03_2024/exercicio02_2.m
@@ -0,0 +1,30 @@
+% Calcular a media de um aluno da UNIVALI
+
+% Dados: Conhecer as três medias parciais
+% Resultado: MF = (M1+M2+M3)/3
+
+% valores de teste
+
+% | Nome  |  M1   |  M2   |  M3   |  MF  |
+% | ------------------------------------ |
+% | Joao  |  8.0  |  4.0  |  6.0  |  6.0 |
+% | Maria |  1.5  |  3.5  |  5.5  |  3.5 |
+% | Aluno |  5.0  |  5.5  |  6.0  |  5.5 |
+
+clear; clc; 
+
+nome = input('Digite o nome do aluno (a): ', 's');
+M1 = input('Digite a nota da sua M1: ');
+M2 = input('Digite a nota da sua M2: ');
+M3 = input('Digite a nota da sua M3: ');
+
+
+soma = M1 + M2 + M3;
+
+mediaFinal = soma / 3; 
+
+if mediaFinal >= 6
+    disp('Aprovado!')
+else
+    disp('Reprovado!')
+end
